29d2dcf5a7e37c10d4c07a0d830f1ca859a1126c
Rahulnegi8192 <rahulnegi8192@gmail.com>
2025-06-30 23:23:01 +0530

    fix:cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const dotenv = require("dotenv");
 
+const cors = require("cors");
+
 const pingRoute = require("./routes/pingRoute");
 
 const app = express();
 app.use(express.json());
+
+app.use(cors());
 
 app.use("/pong", pingRoute);
 
